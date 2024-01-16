eeae4a37af19f98a2ed5c7e270c602a884aceda8
Teikk <nguyenkiet228@gmail.com>
2024-01-16 14:43:43 +0700

    Set Up Retrofit

---
+++ app/src/main/java/com/example/newsapp/News/Models/Source.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.News.Models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/newsapp/News/Utils/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.News.Utils
+
+class Constant {
+    companion object {
+        const val API_KEY = "d63303f11add4e1983126ffc8f0c52bb"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
